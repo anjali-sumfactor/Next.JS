7b4eac1bbc0545e530d2447037ff33c9ec7e2494
anjali-sumfactor <anjali.sumfactor@gmail.com>
2023-05-12 10:27:01 +0530

    store coffee stores in store context

--- discover-coffee-store/pages/_app.js
+++ discover-coffee-store/pages/_app.js
@@ -1,15 +1,35 @@
 import '@/styles/globals.css';
-import { createContext } from 'react';
+import { createContext, useReducer } from 'react';
 
 const StoreContext = createContext();
+
+const ACTION_TYPES = {
+  SET_LAT_LONG: 'SET_LAT_LONG',
+  SET_COFFEE_STORES: 'SET_COFFEE_STORES',
+}
+
+const storeReducer = (state, action) => {
+  switch (action.type) {
+    case ACTION_TYPES.SET_LAT_LONG: {
+      return { ...state, latLong: action.payload.latLong };
+    }
+    case ACTION_TYPES.SET_COFFEE_STORES: {
+      return { ...state, latLong: action.payload.coffeeStores };
+    }
+    default:
+      throw new Error(`Unhandle action type: ${action.type}`);
+  }
+}
 
 const StoreProvider = ({ children }) => {
   const initialState = {
     latLong: "",
     coffeeStores: [],
-  }
+  };
 
-  return <StoreContext.Provider value={{ state: initialState }}>
+  const [state, dispatch] = useReducer(storeReducer, initialState);
+
+  return <StoreContext.Provider value={{ state: dispatch }}>
     {children}
   </StoreContext.Provider>
 
