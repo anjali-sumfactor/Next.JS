efd04b0fa070763d84d0027146ffabd7e555d584
anjali-sumfactor <anjali.sumfactor@gmail.com>
2023-05-16 12:23:28 +0530

    basic coffee store API

--- discover-coffee-store/pages/api/createCoffeeStore.js
+++ discover-coffee-store/pages/api/createCoffeeStore.js
@@ -1,2 +1,16 @@
 const Airtable = require('airtable');
-const base = new Airtable({ apiKey: 'YOUR_SECRET_API_TOKEN' }).base('appIchLOOXgcG3CaU');
+const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process.env.AIRTABLE_BASE_KEY);
+
+const table = base("coffee-stores");
+console.log(table);
+
+const createCoffeeStore = (req, res) => {
+    console.log({ req });
+    if (req.method === 'POST') {
+        res.json({ message: "Hi there" });
+    } else {
+        res.json({ message: "method is GET" });
+    }
+}
+
+export default createCoffeeStore;
