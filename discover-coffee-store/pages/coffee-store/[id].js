f588e96fbb6bd9c6c914dc1340e5c118f6f6e2dd
anjali-sumfactor <anjali.sumfactor@gmail.com>
2023-05-18 17:13:09 +0530

    updated code

--- discover-coffee-store/pages/coffee-store/[id].js
+++ discover-coffee-store/pages/coffee-store/[id].js
@@ -18,7 +18,7 @@ export async function getStaticProps(staticProps) {
     const params = staticProps.params;
     const coffeeStores = await fetchCoffeeStores();
     const findCoffeeStoreById = coffeeStores.find(coffeeStore => {
-        return coffeeStore.id.toString() === params.id; //dynamic id
+        return coffeeStore.id.toString() === params.id;
     });
 
     return {
